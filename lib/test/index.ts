b2b3c366786ec62806313fef107bd52a69891238
rayyreall <19121247@smktelkom-bjb.sch.id>
2022-04-26 21:42:25 +0700

    This project is not finished yet

--- lib/test/index.ts
+++ lib/test/index.ts
@@ -1,17 +1,4 @@
 import path from "path";
 import lodash from "lodash";
-import NodeCache from "node-cache";
-import Command, {Config, Get} from "../events";
-import type Whatsapp from "../types";
-
-class h {
-	constructor() {}
-	public utils: any | undefined;
-	@Get("utils")
-	metode() {
-		console.log(this.utils);
-	}
-}
 (async () => {
-	new h().metode();
 })();
